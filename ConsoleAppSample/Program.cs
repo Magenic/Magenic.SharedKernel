b68c5883b2c5e76fe8c7eb3e1b7bb85c9a3f1956
Geoff Hudik <GeoffH@magenic.com>
2017-07-26 19:43:43 -0700

    Start of Simple Console App Sample; quick SecureRandom use

---
+++ ConsoleAppSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Magenic.SharedKernel;
+
+namespace ConsoleAppSample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (SecureRandom sr = SecureRandom.Create())
+            {
+                Console.WriteLine($"Random number: {sr.Next()}");
+            }
+        }
+    }
+}
